b5156b5aaf8cd00aecd04ababdd1b3a11d51b3ce
Cristopher <cristopherdered1@email.com>
2023-11-11 04:02:10 -0600

    Payments

--- src/firebase/cloudstorage/Children.js
+++ src/firebase/cloudstorage/Children.js
@@ -1,20 +1,32 @@
-import { doc, setDoc } from "firebase/firestore";
-import { collection, addDoc } from "firebase/firestore"; 
+import { doc, getDoc, setDoc } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import { db } from "../firebase";
 
 //funcion crear un usuario con el uid
-export const createChildWUID = async(data, UID) => {
-    await setDoc(doc(db, "Children", UID), {...data});
-    return {...data, "userUID" : UID } 
+export const createChildWUID = async (data, UID) => {
+  await setDoc(doc(db, "Children", UID), { ...data });
+  return { ...data, "userUID": UID }
 }
 
 //funcion crear un usuario con el uid
-export const createChild = async(data) => {
-    // Add a new document with a generated id.
-    const docRef = await addDoc(collection(db, "Children"), {
-      ...data
-    });
-    return docRef.id
+export const createChild = async (data) => {
+  // Add a new document with a generated id.
+  const docRef = await addDoc(collection(db, "Children"), {
+    ...data
+  });
+  return docRef.id
 }
 
 
+//Funcion para traerse la informacion de un niño
+
+export const getDataChild = async (child_id) => {
+  const childrenRef = doc(db, "Children", child_id);
+  const docSnapshot = await getDoc(childrenRef);
+  if (docSnapshot.exists()) {
+    return { ...docSnapshot.data(), id_user: child_id }
+  } else {
+    return { noting: "nothing" }
+  }
+
+}
--- src/screens/user/Payments_Screen.js
+++ src/screens/user/Payments_Screen.js
@@ -1,28 +1,26 @@
 import { View, Text, TouchableOpacity } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useLayoutEffect, useState } from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { getData } from '../../Storage/storage'
 import { db } from '../../firebase/firebase'
 import { doc, getDoc, onSnapshot } from 'firebase/firestore'
 import ModalLoading from '../../componentes/loading/loading'
+import { getDataChild } from '../../firebase/cloudstorage/Children'
 
 const PaymentsScreen = ({ navigation }) => {
     const [userData, setUserData] = useState(null);
-    const [childNames, setChildNames] = useState([])
     const [isLoading, setIsLoading] = useState(true)
+    const [childNames, setChildNames] = useState([])
 
     useEffect(() => {
-       setIsLoading(true) 
+        setIsLoading(true)
         getData()
             .then((data) => {
-                setUserData(data)
                 // Consulatamos la BD para traernos los datos del usuario
-                onSnapshot(doc(db, "Usuarios", userData.userUID), (doc) => {
+                onSnapshot(doc(db, "Usuarios", data.userUID), (doc) => {
                     setUserData({ ...doc.data() })
-                    fetchChildNames()
-                    setIsLoading(false)
                 })
-
+                fetchChildNames()
             })
             .catch((error) => {
                 setIsLoading(false)
@@ -36,58 +34,80 @@ const PaymentsScreen = ({ navigation }) => {
 
     //Obtener los nombre de los hijes
     const fetchChildNames = async () => {
-        setIsLoading(true) 
-        const childNamesArray = [];
-        //Verifica si userData existe y si tiene una propiedad hijos_matricula que es un array no vacío
-        if (userData && userData.hijos_matricula.length >= 0) {
-            // Itera a través de los IDs de los hijos_matricula
-            for (const childId of userData.hijos_matricula) {
-                const childrenRef = doc(db, "Children", childId);
-                try {
-                    const docSnapshot = await getDoc(childrenRef);
-                    // agrega sus datos al array childNamesArray
-                    if (docSnapshot.exists()) {
-                        childNamesArray.push({ ...docSnapshot.data(), id_user: childId });
-                    }
-                } catch (error) {
+        setIsLoading(true)
+        const childrenInfo = []
+        userData.hijos_matricula.forEach(childID => {
+            getDataChild(childID)
+                .then((childInfo) => {
+                    childrenInfo.push(childInfo)
+                    console.log(childInfo)
+                })
+                .catch((error) => {
                     setIsLoading(false)
-                    console.error('Error al obtener el documento: ', error);
-                    console.log(childNamesArray)
-                }
-            }
-        }
-        // Agrega tota la informacion al array para que el papi tmb pueda
-        childNamesArray.push({ ...userData });
-        setChildNames(childNamesArray);
+                    const errorCode = error.code;
+                    const errorMessage = error.message;
+                    console.log('useLayoutEffect - onSnapshot ', errorCode, ' ', errorMessage)
+                })
+        });
+        setChildNames(childrenInfo)
         setIsLoading(false)
     };
 
 
-  useEffect(() => {
-    fetchChildNames();
-  },[])
 
-  return (
-    <View>
-      <StatusBar hidden={true} />
-      <Text className="mb-6 mt-4 ml-7 text-lg">Mensualidad</Text>
-      {childNames.map((child, index) => {
-        if (child.id_user || child.userUID) { // Comprueba si hay un nombre de usuario
-          return (
-            <TouchableOpacity
-              key={index}
-              onPress={() => navigation.navigate("Inscripcion", child)}
-              className="rounded-md bg-baseDark h-10 justify-center ml-7 mr-7 mb-4 items-center"
-            >
-              <Text className="w-80 text-center">Inscripcion de {child.name_user}</Text>
-            </TouchableOpacity>
-          );
-        }else
-          return null; // No tiene nombre de usuario, no se muestra el botón
-      })}
-    </View>
-  );
-  
+
+    return (
+        <View>
+            <StatusBar hidden={true} />
+            <Text className="mb-6 mt-4 ml-7 text-lg">Mensualidad</Text>
+
+            {
+                isLoading
+                    ? <ModalLoading visible={isLoading} />
+                    : <>
+                        <TouchableOpacity
+                            onPress={() => { }}
+                            className="rounded-md bg-baseDark h-10 justify-center ml-7 mr-7 mb-4 items-center">
+                            <Text className="w-80 text-center">
+                                Inscripcion de {userData.name_user} {userData.pattern_name} {userData.matern_name}
+                            </Text>
+                        </TouchableOpacity>
+                        {
+                            console.log(childNames.length)
+                        }
+
+                        {
+                            childNames.length > 0
+                                ?
+                                <View>
+                                    {
+                                        childNames.map((info, indice) => {
+                                            console.log(info)
+                                            return (
+                                                <TouchableOpacity
+                                                    key={indice}
+                                                    onPress={() => { }}
+                                                    className="rounded-md bg-baseDark h-10 justify-center ml-7 mr-7 mb-4 items-center">
+                                                    <Text className="w-80 text-center">
+                                                        Inscripcion de {info.name_user} {info.pattern_name} {info.matern_name}
+                                                    </Text>
+                                                </TouchableOpacity>
+                                            )
+                                        })
+                                    }
+                                </View>
+
+                                :
+                                <Text>no Hijo</Text>
+                        }
+
+                    </>
+            }
+
+
+        </View>
+    );
+
 
 }
 
